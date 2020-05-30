6c9ade6fd246c161c29d44101d030b62999f6b2f
Robert Marks <rmarks@nav.com>
2020-05-30 09:33:27 -0600

    initial commit

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,11 @@
--- TODO:
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  burger_name varchar(255) NOT NULL,
+  devoured boolean
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,4 @@
--- TODO:
+INSERT INTO burgers (burger_name, devoured) values ('Poutine on the Ritz Burger', false)
+INSERT INTO burgers (burger_name, devoured) values ('Poblano Picasso Burger', true)
+INSERT INTO burgers (burger_name, devoured) values ("Don't You Four Cheddar 'Bout Me Burger", true)
+INSERT INTO burgers (burger_name, devoured) values ('Chipotle Off the Old Block Burger', true)
